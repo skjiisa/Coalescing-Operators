a43b3e4e07c98b233b3eb58cef46eda349be4a81
Elaine Lyons <lyons@tuta.io>
2022-12-05 22:34:53 -0700

    Test remaining ???

--- Tests/Coalescing OperatorsTests/Coalescing_OperatorsTests.swift
+++ Tests/Coalescing OperatorsTests/Coalescing_OperatorsTests.swift
@@ -59,4 +59,54 @@ final class Coalescing_OperatorsTests: XCTestCase {
         XCTAssertFalse(defaultCalled)
     }
     
+    //MARK: - ??? non-optional, non-optional
+    
+    func testEmptyCoalescingNonOptionalNonOptionalEmptyLeft() {
+        let expected: [Int] = [1, 2, 3]
+        let actual: [Int] = [] ??? expected
+        XCTAssertEqual(actual, expected)
+    }
+    
+    func testEmptyCoalescingNonOptionalNonOptionalValidLeft() {
+        var defaultCalled = false
+        func defaultValue() -> [Int] {
+            defaultCalled = true
+            return [1, 2, 3, 4, 5]
+        }
+        
+        let expected: [Int] = [1, 2, 3]
+        let actual: [Int] = expected ??? defaultValue()
+        
+        XCTAssertEqual(actual, expected)
+        XCTAssertFalse(defaultCalled)
+    }
+    
+    //MARK: ??? non-optonal, optional
+    
+    func testEmptyCoalescingNonOptionalOptionalEmptyLeftValidRight() {
+        let expected: [Int]? = [1, 2, 3]
+        let actual: [Int] = [] ??? expected
+        XCTAssertEqual(actual, expected)
+    }
+    
+    func testEmptyCoalescingNonOptionalOptionalEmptyLeftNilRight() {
+        let expected: [Int] = []
+        let actual: [Int] = expected ??? nil
+        XCTAssertEqual(actual, expected)
+    }
+    
+    func testEmptyCoalescingNonOptionalOptionalValidLeft() {
+        var defaultCalled = false
+        func defaultValue() -> [Int]? {
+            defaultCalled = true
+            return [1, 2, 3, 4, 5]
+        }
+        
+        let expected: [Int] = [1, 2, 3]
+        let actual: [Int] = expected ??? defaultValue()
+        
+        XCTAssertEqual(actual, expected)
+        XCTAssertFalse(defaultCalled)
+    }
+    
 }
